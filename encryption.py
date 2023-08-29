d9da9e82f8749bf91cbe8ba77947709898168c40
bishal <46195146+bishal-dhakal@users.noreply.github.com>
2023-08-29 09:43:53 +0545

    Update encryption.py

--- encryption.py
+++ encryption.py
@@ -1,9 +1,37 @@
+#!/bin/python3
+
 import math
+import os
+import random
+import re
+import sys
 
-def encryption():
+#
+# Complete the 'encryption' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def encryption(s):
     # Write your code here
-    s = "if man was meant to stay on the groung god would have given us roots"
-    
+    s= s.replace(' ','')
+    n= len(s)
+    x = math.sqrt(n)
+    u = math.ceil(x)
 
+    word = ""
+    for i in range(u):
+        word += s[i: :u] + " "
+    return word
 
-encryption()
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = encryption(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
